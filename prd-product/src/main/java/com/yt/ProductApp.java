0c4026a0e89149dfaa29dbe509af9feb4bc2cac0
yt <1686706489@qq.com>
2023-10-23 14:12:01 +0800

    1023 14:11

---
+++ prd-product/src/main/java/com/yt/ProductApp.java
@@ -0,0 +1,17 @@
+package com.yt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Hello world!
+ *
+ */
+@SpringBootApplication
+public class ProductApp
+{
+    public static void main( String[] args )
+    {
+        SpringApplication.run(ProductApp.class,args);
+    }
+}
--- prd-user/src/main/java/com/yt/controller/UserController.java
+++ prd-user/src/main/java/com/yt/controller/UserController.java
@@ -2,23 +2,28 @@ package com.yt.controller;
 
 import com.yt.entity.User;
 import com.yt.vo.ResponseResult;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import com.yt.service.LoginService;
 @RestController
 @RequestMapping("/user")
+@Api(value = "用户接口", tags = "用户接口")
 public class UserController {
     @Autowired
     private LoginService loginService;
 
     @PostMapping("/login")
     @ResponseBody
+    @ApiOperation(value = "登录", tags = "登录")
     public ResponseResult login(@RequestBody User user){
         return loginService.login(user);
     }
 
     @GetMapping("/logout")
     @ResponseBody
+    @ApiOperation(value = "登出", tags = "登出")
     public ResponseResult logout(){
         return loginService.logout();
     }
