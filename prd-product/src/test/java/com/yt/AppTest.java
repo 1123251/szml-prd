ea3c604ae888cd37181fbdf9991182e7b44378b0
yt <1686706489@qq.com>
2023-10-26 01:45:42 +0800

    1026 1:43 商品上线下线，多条件分页查询

--- prd-product/src/test/java/com/yt/AppTest.java
+++ prd-product/src/test/java/com/yt/AppTest.java
@@ -28,8 +28,8 @@ public class AppTest
             System.out.println(date);
             //Fri Nov 29 10:05:00 CST 2019
             SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-            selectRulers.setStart_time(sdf.parse("2009-12-31 00:00:00"));
-            selectRulers.setEnd_time(sdf.parse("2025-12-31 00:00:00"));
+            selectRulers.setStartTime(sdf.parse("2009-12-31 00:00:00"));
+            selectRulers.setEndTime(sdf.parse("2025-12-31 00:00:00"));
             System.out.println(productMapper.selectByRulers(selectRulers));
         }
 }
