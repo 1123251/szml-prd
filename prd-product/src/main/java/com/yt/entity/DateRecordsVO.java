11fa36519a9fb94f359d1ec166eca9288596da6b
yt <1686706489@qq.com>
2023-10-27 17:53:21 +0800

    1027 13:56

--- prd-product/src/main/java/com/yt/entity/DateRecordsVO.java
+++ prd-product/src/main/java/com/yt/entity/DateRecordsVO.java
@@ -7,7 +7,7 @@ import java.util.Date;
 
 @Data
 public class DateRecordsVO {
-
+    @JsonFormat(timezone = "GMT+8")
     private Date time;
     private int nums;
 }
--- prd-product/src/main/java/com/yt/entity/TimePeriod.java
+++ prd-product/src/main/java/com/yt/entity/TimePeriod.java
@@ -1,5 +1,6 @@
 package com.yt.entity;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 import org.springframework.format.annotation.DateTimeFormat;
 
@@ -7,6 +8,8 @@ import java.util.Date;
 
 @Data
 public class TimePeriod {
+    @JsonFormat(timezone =  "GMT+8")
     private Date startTime;
+    @JsonFormat(timezone =  "GMT+8")
     private Date endTime;
 }
